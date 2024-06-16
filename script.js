059f2f947053d2f8d6b94a13709ea3868734e64b
ajoshi103 <100084710+ajoshi103@users.noreply.github.com>
2024-06-16 17:18:01 +0530

    Done

---
+++ script.js
@@ -0,0 +1,16 @@
+function change(){
+
+
+  var h1 = document.getElementById("nav");
+  h1.classList.add("addcolor");
+    document.getElementById("nav").style.color = "white";
+    
+
+ }
+
+
+
+
+
+
+
